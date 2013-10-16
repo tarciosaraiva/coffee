f5b5930a580007bd5737e34e93a55f069f14f235
Tarcio T Saraiva <tarcio@gmail.com>
2013-10-17 09:59:01 +1100

    Adding client and transaction creation

--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -3,8 +3,9 @@ package controllers
 import models._
 import play.api.mvc._
 import play.api.libs.json._
-import play.filters.csrf._
 import play.api.libs.functional.syntax._
+import org.joda.time.{DateTime, LocalDate}
+import anorm.Id
 
 object Api extends Controller {
 
@@ -13,82 +14,84 @@ object Api extends Controller {
   implicit val addClientReads = (
     (__ \ 'name).read[String] and
       (__ \ 'balance).read[BigDecimal] and
-      (__ \ 'coffee).read[String] and
-      (__ \ 'milk).read[String] and
-      (__ \ 'addTransaction).read[Boolean] and
-      (__ \ 'goToCustomerRecord).read[Boolean]
+      (__ \ 'email).read[String] and
+      (__ \ 'twitter).read[String] and
+      (__ \ 'dob).read[LocalDate] and
+      (__ \ 'addTransaction).read[Boolean]
     ) tupled
 
   implicit val addTransactionReads = (
     (__ \ 'amount).read[BigDecimal] and
-      (__ \ 'coffee).read[String] and
-      (__ \ 'milk).read[String] and
+      (__ \ 'notes).read[String] and
       (__ \ 'credit).read[Boolean]
     ) tupled
 
-  def search = CSRFCheck {
-    Action(parse.json) {
-      request =>
-        request.body.validate[(String)].map {
-          case (term) => {
-            val result = Client.findByName(term)
-            Ok(Json.obj("__total" -> result.size, "data" -> result.map(_.toJson)))
-          }
-        }.recoverTotal {
-          e => BadRequest(Json.obj("status" -> "NOK", "message" -> JsError.toFlatJson(e)))
+  def search = Action(parse.json) {
+    request =>
+      request.body.validate[(String)].map {
+        case (term) => {
+          val result = Client.findByName(term)
+          Ok(Json.obj("__total" -> result.size, "data" -> result.map(_.toJson)))
         }
-    }
+      }.recoverTotal {
+        e => BadRequest(Json.obj("status" -> "NOK", "message" -> JsError.toFlatJson(e)))
+      }
   }
 
-  def indexes = CSRFCheck {
-    Action {
-      request =>
-        val result = Client.indexes
-        Ok(Json.obj("__total" -> result.size, "data" -> result))
-    }
+  def indexes = Action {
+    request =>
+      val result = Client.indexes
+      Ok(Json.obj("__total" -> result.size, "data" -> result))
   }
 
-  def index(index: String) = CSRFCheck {
-    Action {
-      request =>
-        val result = Client.indexes(index)
-        Ok(Json.obj("__total" -> result.size, "data" -> result.map(_.toJson)))
-    }
+  def index(index: String) = Action {
+    request =>
+      val result = Client.indexes(index)
+      Ok(Json.obj("__total" -> result.size, "data" -> result.map(_.toJson)))
   }
 
-  def client(id: Long) = CSRFCheck {
-    Action {
-      request =>
-        val client = Client.findOne(id).get
-        val transactions = Transaction.allByClient(id)
-        Ok(Json.obj("client" -> client.toJson, "transactions" -> transactions.map(_.toJson)))
-    }
+  def client(id: Long) = Action {
+    request =>
+      val client = Client.findOne(id).get
+      val transactions = Transaction.allByClient(id)
+      Ok(Json.obj("client" -> client.toJson, "transactions" -> transactions.map(_.toJson)))
   }
 
-  def addClient = CSRFCheck {
-    Action(parse.json) {
-      request =>
-        request.body.validate[(String, BigDecimal, String, String, Boolean, Boolean)].map {
-          case (name, balance, coffee, milk, addTransaction, goToCustomerRecord) => {
-            Ok(Json.obj("__total" -> 0))
-          }
-        }.recoverTotal {
-          e => BadRequest(Json.obj("status" -> "NOK", "message" -> JsError.toFlatJson(e)))
+  def addClient = Action(parse.json) {
+    request =>
+      request.body.validate[(String, BigDecimal, String, String, LocalDate, Boolean)].map {
+        case (name, balance, email, twitter, dob, addTransaction) => {
+          // add the client
+          val newClientId = Client.create(Client(Id(1), name, BigDecimal(0), email, twitter, dob)).get
+
+          // add two transactions, one for the top-up
+          Transaction.create(Transaction(DateTime.now, true, balance, "", newClientId))
+
+          // and the other for the coffee
+          if (addTransaction)
+            Transaction.create(Transaction(DateTime.now, false, BigDecimal(-3.4), "Coffee", newClientId))
+
+          // and then updates the client
+          Client.updateBalance(newClientId)
+
+          Ok(Json.obj("id" -> newClientId))
         }
-    }
+      }.recoverTotal {
+        e => BadRequest(Json.obj("status" -> "NOK", "message" -> JsError.toFlatJson(e)))
+      }
   }
 
-  def addTransaction(id: Long) = CSRFCheck {
-    Action(parse.json) {
-      request =>
-        request.body.validate[(BigDecimal, String, String, Boolean)].map {
-          case (amount, coffee, milk, credit) => {
-            Ok(Json.obj("__total" -> 0))
-          }
-        }.recoverTotal {
-          e => BadRequest(Json.obj("status" -> "NOK", "message" -> JsError.toFlatJson(e)))
+  def addTransaction(id: Long) = Action(parse.json) {
+    request =>
+      request.body.validate[(BigDecimal, String, Boolean)].map {
+        case (amount, notes, credit) => {
+          Transaction.create(Transaction(DateTime.now, credit, amount, notes, id))
+          Client.updateBalance(id)
+          Ok
         }
-    }
+      }.recoverTotal {
+        e => BadRequest(Json.obj("status" -> "NOK", "message" -> JsError.toFlatJson(e)))
+      }
   }
 
 }
