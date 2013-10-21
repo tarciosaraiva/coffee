ce111879bf59c4a1013c5c0046924e309b8a6d26
Tarcio T Saraiva <tarcio@gmail.com>
2013-10-21 21:10:25 +1100

    Adding security to Settings

--- app/controllers/Settings.scala
+++ app/controllers/Settings.scala
@@ -16,29 +16,29 @@ object Settings extends Controller with Secured {
 
   implicit val settings: Seq[Setting] = Setting.all
 
-  def show = Action {
-    implicit request =>
+  def show = IsAuthenticated {
+    user => implicit request =>
       Ok(views.html.settings(creationForm, Setting.all))
   }
 
-  def create = Action {
-    implicit request =>
+  def create = IsAuthenticated {
+    user => implicit request =>
       creationForm.bindFromRequest.fold(
         formWithErrors => BadRequest(views.html.settings(creationForm, Setting.all)),
         setting => Redirect(routes.Settings.show)
       )
   }
 
-  def update(id: Long) = Action {
-    implicit request =>
+  def update(id: Long) = IsAuthenticated {
+    user => implicit request =>
       creationForm.bindFromRequest.fold(
         formWithErrors => BadRequest(views.html.settings(creationForm, Setting.all)),
         setting => Redirect(routes.Settings.show)
       )
   }
 
-  def delete(id: Long) = Action {
-    implicit request => {
+  def delete(id: Long) = IsAuthenticated {
+    user => implicit request => {
       Setting.delete(id)
       Redirect(routes.Settings.show)
     }
