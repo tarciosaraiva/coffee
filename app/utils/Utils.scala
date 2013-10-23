a69e776e91571ae9799cdb06cd21e7f39864429a
Tarcio T Saraiva <tarcio@gmail.com>
2013-10-23 21:09:06 +1100

    Forcing timezone with JODA

--- app/utils/Utils.scala
+++ app/utils/Utils.scala
@@ -5,16 +5,13 @@ import anorm.{ToStatement, TypeDoesNotMatch, MetaDataItem, Column}
 import org.joda.time._
 import java.text.{SimpleDateFormat, NumberFormat}
 import models.{Client, Transaction}
-import java.util.TimeZone
 
 object Utils {
 
   def formatNumber(amount: BigDecimal) = NumberFormat.getCurrencyInstance.format(amount)
 
   def formatDateTime(dateTime: DateTime) = {
-    def sdf = new SimpleDateFormat("dd MMM h:mm a")
-    sdf.setTimeZone(TimeZone.getTimeZone("Australia/Melbourne"))
-    sdf.format(dateTime.toDate)
+    new SimpleDateFormat("dd MMM h:mm a").format(dateTime.withZone(DateTimeZone.forID("Australia/Melbourne")).toDate)
   }
 
   def isBirthday(birthDate: Option[LocalDate]): Boolean = {
